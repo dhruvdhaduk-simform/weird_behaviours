75c3fbba86b982b903ad2a733c45c0e2186482af
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-02 11:48:39 +0530

    [typescript] Add 02_generics_and_exclude.ts

---
+++ typescript/02_generics_and_exclude.ts
@@ -0,0 +1,19 @@
+
+/**
+ * Exclude type with vs without generics.
+ *
+ * Exclude does not work when we remove generics from it.
+ */
+
+// type Y = Exclude<any, any>
+
+type A = 'a' | 'b' | 'c' | 'd';
+type X = 'b' | 'd';
+
+type _Exclude<T, U> = T extends U ? never : T;    // Same as Exclude utility type.
+
+type B1 = _Exclude<A, X>;   // Works
+// Use the same _Exclude logic but without generics.
+type B2 = A extends X ? never : A;      // Doesn't work
+
+
